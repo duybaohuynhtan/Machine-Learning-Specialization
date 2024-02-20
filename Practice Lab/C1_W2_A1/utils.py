0f8b0fee07e7156b2413889cc0f5c151a914f0ae
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-02-21 02:31:23 +0900

    C1_W3_Lab08

--- Practice Lab/C1_W2_A1/utils.py
+++ Practice Lab/C1_W2_A1/utils.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def load_data():
-    data = np.loadtxt("data/ex1data1.txt", delimiter=',')
+    data = np.loadtxt("D:\\Machine Learning Specialization\\Labs\\Practice Lab\\C1_W2_A1\\data\\ex1data1.txt", delimiter=',')
     X = data[:,0]
     y = data[:,1]
     return X, y
 
 def load_data_multi():
-    data = np.loadtxt("data/ex1data2.txt", delimiter=',')
+    data = np.loadtxt("D:\\Machine Learning Specialization\\Labs\\Practice Lab\\C1_W2_A1\\data\\ex1data2.txt", delimiter=',')
     X = data[:,:2]
     y = data[:,2]
     return X, y
