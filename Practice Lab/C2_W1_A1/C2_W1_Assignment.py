a7efe6d5993961824a43bde983904519c0bc7d4b
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-02-26 17:08:28 +0900

    C2_W1_A1

--- Practice Lab/C2_W1_A1/C2_W1_Assignment.py
+++ Practice Lab/C2_W1_A1/C2_W1_Assignment.py
@@ -4,8 +4,46 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
 from autils import *
-%matplotlib inline
-
 import logging
+import warnings       
+
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
+
+# load dataset
+X, y = load_data()
+
+print ('The shape of X is: ' + str(X.shape))
+print ('The shape of y is: ' + str(y.shape))
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1)
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Display the label above the image
+    ax.set_title(y[random_index,0])
+    ax.set_axis_off()
+
+model = Sequential(
+    [               
+        tf.keras.Input(shape=(400,)),    #specify input size
+        ### START CODE HERE ### 
+        tf.keras.layers.Dense(25, activation="sigmoid"),
+        tf.keras.layers.Dense(15, activation="sigmoid"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+        ### END CODE HERE ### 
+    ], name = "my_model" 
+) 
