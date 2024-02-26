b097ad14beee71b7c4a582386c5fc469ea5c84c5
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-02-26 14:02:01 +0900

    C2_W1_Lab03

---
+++ Optional Labs/C2_W1/C2_W1_Lab03_CoffeeRoasting_Numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+import tensorflow as tf
+from lab_utils_common import dlc, sigmoid
+from lab_coffee_utils import load_coffee_data, plt_roast, plt_prob, plt_layer, plt_network, plt_output_unit
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
