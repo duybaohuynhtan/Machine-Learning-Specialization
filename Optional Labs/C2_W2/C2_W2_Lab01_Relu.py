466aa1260acb0a1c467923e98a3f399584401704
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-02-28 12:45:33 +0900

    C2_W2_Lab01

--- Optional Labs/C2_W2/C2_W2_Lab01_Relu.py
+++ Optional Labs/C2_W2/C2_W2_Lab01_Relu.py
@@ -6,10 +6,11 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LeakyReLU
 from tensorflow.keras.activations import linear, relu, sigmoid
-%matplotlib widget
 from matplotlib.widgets import Slider
 from lab_utils_common import dlc
 from autils import plt_act_trio
 from lab_utils_relu import *
 import warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
+
+plt_act_trio()
