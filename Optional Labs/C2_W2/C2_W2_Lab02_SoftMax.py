d89eae452508ce1bfd063b98b10ec8728e8d7943
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-02-29 16:00:39 +0900

    C2_W2_Lab02

---
+++ Optional Labs/C2_W2/C2_W2_Lab02_SoftMax.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from IPython.display import display, Markdown, Latex
+from sklearn.datasets import make_blobs
+from matplotlib.widgets import Slider
+from lab_utils_common import dlc
+from lab_utils_softmax import plt_softmax
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
