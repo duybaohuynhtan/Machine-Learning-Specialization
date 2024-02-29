356bda4df2d81e56fa0e85efab9748c9d6f2e41b
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-01 01:45:02 +0900

    C2_W2_A1

--- Practice Lab/C2_W2_A1/C2_W2_Neural_Networks.py
+++ Practice Lab/C2_W2_A1/C2_W2_Neural_Networks.py
@@ -11,7 +11,120 @@ logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
 
 from public_tests import * 
-
 from autils import *
 from lab_utils_softmax import plt_softmax
 np.set_printoptions(precision=2)
+
+def my_softmax(z):  
+    """ Softmax converts a vector of values to a probability distribution.
+    Args:
+      z (ndarray (N,))  : input data, N features
+    Returns:
+      a (ndarray (N,))  : softmax of z
+    """    
+    ez = np.exp(z)
+    a = ez/np.sum(ez)
+    return a
+
+# load dataset
+X, y = load_data()
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(5,5))
+fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.91]) #[left, bottom, right, top]
+
+#fig.tight_layout(pad=0.5)
+widgvis(fig)
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Display the label above the image
+    ax.set_title(y[random_index,0])
+    ax.set_axis_off()
+    fig.suptitle("Label, image", fontsize=14)
+
+tf.random.set_seed(1234) # for consistent results
+model = Sequential(
+    [               
+        tf.keras.layers.InputLayer((400,)),
+        tf.keras.layers.Dense(25, activation="relu", name="L1"),
+        tf.keras.layers.Dense(15, activation="relu", name="L2"),
+        tf.keras.layers.Dense(10, activation="linear", name="L3")
+    ], name = "my_model" 
+)
+
+model.summary()
+
+model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+)
+
+history = model.fit(
+    X,y,
+    epochs=40
+)
+
+plot_loss_tf(history)
+
+image_of_two = X[1015]
+display_digit(image_of_two)
+
+prediction = model.predict(image_of_two.reshape(1,400))  # prediction
+
+print(f" predicting a Two: \n{prediction}")
+print(f" Largest Prediction index: {np.argmax(prediction)}")
+
+prediction_p = tf.nn.softmax(prediction)
+
+print(f" predicting a Two. Probability vector: \n{prediction_p}")
+print(f"Total of predictions: {np.sum(prediction_p):0.3f}")
+
+yhat = np.argmax(prediction_p)
+
+print(f"np.argmax(prediction_p): {yhat}")
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(5,5))
+fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.91]) #[left, bottom, right, top]
+widgvis(fig)
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Predict using the Neural Network
+    prediction = model.predict(X[random_index].reshape(1,400))
+    prediction_p = tf.nn.softmax(prediction)
+    yhat = np.argmax(prediction_p)
+    
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]},{yhat}",fontsize=10)
+    ax.set_axis_off()
+fig.suptitle("Label, yhat", fontsize=14)
+plt.show()
+
+print( f"{display_errors(model,X,y)} errors out of {len(X)} images")
