2139582827ba495e9c69cc854029184d9e1d7ffc
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-06 14:46:05 +0900

    C2_W3_A1

--- Practice Lab/C2_W3_A1/C2_W3_Model_Complexity.py
+++ Practice Lab/C2_W3_A1/C2_W3_Model_Complexity.py
@@ -20,3 +20,63 @@ tf.keras.backend.set_floatx('float64')
 from assigment_utils import *
 
 tf.autograph.set_verbosity(0)
+
+tf.random.set_seed(1234)
+model = Sequential(
+    [
+        tf.keras.layers.Dense(120, activation="relu"),
+        tf.keras.layers.Dense(40, activation="relu"),
+        tf.keras.layers.Dense(6, activation="linear")
+    ], name="Complex"
+)
+model.compile(
+    loss=SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(lr=0.01),
+)
+
+model.fit(
+    X_train, y_train,
+    epochs=1000
+)
+
+model.summary()
+
+#make a model for plotting routines to call
+model_predict = lambda Xl: np.argmax(tf.nn.softmax(model.predict(Xl)).numpy(),axis=1)
+plt_nn(model_predict,X_train,y_train, classes, X_cv, y_cv, suptitle="Complex Model")
+
+training_cerr_complex = eval_cat_err(y_train, model_predict(X_train))
+cv_cerr_complex = eval_cat_err(y_cv, model_predict(X_cv))
+print(f"categorization error, training, complex model: {training_cerr_complex:0.3f}")
+print(f"categorization error, cv,       complex model: {cv_cerr_complex:0.3f}")
+
+tf.random.set_seed(1234)
+model_s = Sequential(
+    [
+        tf.keras.layers.Dense(6, activation="relu"),
+        tf.keras.layers.Dense(6, activation="linear")
+    ], name = "Simple"
+)
+model_s.compile(
+    loss=SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(lr=0.01),
+)
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+model_s.fit(
+    X_train,y_train,
+    epochs=1000
+)
+
+model_s.summary()
+
+#make a model for plotting routines to call
+model_predict_s = lambda Xl: np.argmax(tf.nn.softmax(model_s.predict(Xl)).numpy(),axis=1)
+plt_nn(model_predict_s,X_train,y_train, classes, X_cv, y_cv, suptitle="Simple Model")
+
+training_cerr_simple = eval_cat_err(y_train, model_predict_s(X_train))
+cv_cerr_simple = eval_cat_err(y_cv, model_predict_s(X_cv))
+print(f"categorization error, training, simple model, {training_cerr_simple:0.3f}, complex model: {training_cerr_complex:0.3f}" )
+print(f"categorization error, cv,       simple model, {cv_cerr_simple:0.3f}, complex model: {cv_cerr_complex:0.3f}" )
