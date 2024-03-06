808a8455d3e0f04d3b6ee4915a9e925e602c9694
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-06 14:52:47 +0900

    C2_W3_A1

--- Practice Lab/C2_W3_A1/C2_W3_Regularization.py
+++ Practice Lab/C2_W3_A1/C2_W3_Regularization.py
@@ -20,3 +20,34 @@ tf.keras.backend.set_floatx('float64')
 from assigment_utils import *
 
 tf.autograph.set_verbosity(0)
+
+tf.random.set_seed(1234)
+model_r = Sequential(
+    [
+        tf.keras.layers.Dense(120, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.1)),
+        tf.keras.layers.Dense(40, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.1)),
+        tf.keras.layers.Dense(6, activation="linear")
+    ], name= None
+)
+model_r.compile(
+    loss=SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(lr=0.01),
+)
+
+model_r.fit(
+    X_train, y_train,
+    epochs=1000
+)
+
+model_r.summary()
+
+#make a model for plotting routines to call
+model_predict_r = lambda Xl: np.argmax(tf.nn.softmax(model_r.predict(Xl)).numpy(),axis=1)
+ 
+plt_nn(model_predict_r, X_train,y_train, classes, X_cv, y_cv, suptitle="Regularized")
+
+training_cerr_reg = eval_cat_err(y_train, model_predict_r(X_train))
+cv_cerr_reg = eval_cat_err(y_cv, model_predict_r(X_cv))
+test_cerr_reg = eval_cat_err(y_test, model_predict_r(X_test))
+print(f"categorization error, training, regularized: {training_cerr_reg:0.3f}, simple model, {training_cerr_simple:0.3f}, complex model: {training_cerr_complex:0.3f}" )
+print(f"categorization error, cv,       regularized: {cv_cerr_reg:0.3f}, simple model, {cv_cerr_simple:0.3f}, complex model: {cv_cerr_complex:0.3f}" )
