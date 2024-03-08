b93f8d1768e49222775144804b3a361d8fec3db5
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-08 21:21:42 +0900

    C3_W1_A1

--- Practice Lab/C3_W1_A1/C3_W1_Implementing_K-means.py
+++ Practice Lab/C3_W1_A1/C3_W1_Implementing_K-means.py
@@ -1,3 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
+
+def find_closest_centroids(X, centroids):
+    """
+    Computes the centroid memberships for every example
+    
+    Args:
+        X (ndarray): (m, n) Input values      
+        centroids (ndarray): k centroids
+    
+    Returns:
+        idx (array_like): (m,) closest centroids
+    
+    """
+
+    # Set K
+    K = centroids.shape[0]
+
+    # You need to return the following variables correctly
+    idx = np.zeros(X.shape[0], dtype=int)
+
+    ### START CODE HERE ###
+    for i in range(X.shape[0]):
+          # Array to hold distance between X[i] and each centroids[j]
+          distance = [] 
+          for j in range(centroids.shape[0]):
+              norm_ij = np.linalg.norm(X[i] - centroids[j])
+              distance.append(norm_ij)
+
+          idx[i] = np.argmin(distance)
+
+    ### END CODE HERE ###
+    
+    return idx
