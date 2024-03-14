2ef96a32ee43839b4c475ae78db2a07300d51861
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-15 00:54:02 +0700

    C2_W2_Lab04

---
+++ Optional Labs/C2_W2/C2_W2_Lab04_Derivatives.py
@@ -0,0 +1,20 @@
+from sympy import symbols, diff
+
+J = (3)**2
+J_epsilon = (3 + 0.001)**2
+k = (J_epsilon - J)/0.001    # difference divided by epsilon
+print(f"J = {J}, J_epsilon = {J_epsilon}, dJ_dw ~= k = {k:0.6f} ")
+
+J = (3)**2
+J_epsilon = (3 + 0.000000001)**2
+k = (J_epsilon - J)/0.000000001
+print(f"J = {J}, J_epsilon = {J_epsilon}, dJ_dw ~= k = {k} ")
+
+J, w = symbols('J, w')
+J=w**2
+J
+dJ_dw = diff(J,w)
+dJ_dw
+dJ_dw.subs([(w,2)])    # derivative at the point w = 2
+dJ_dw.subs([(w,3)])    # derivative at the point w = 3
+dJ_dw.subs([(w,-3)])    # derivative at the point w = -3
