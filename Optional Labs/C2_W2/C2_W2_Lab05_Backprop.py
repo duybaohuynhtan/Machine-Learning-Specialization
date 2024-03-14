bcb3399eee818d3bc70116f1d2873870f83b3e8b
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-15 00:59:32 +0700

    C2_W2_Lab05

---
+++ Optional Labs/C2_W2/C2_W2_Lab05_Backprop.py
@@ -0,0 +1,8 @@
+from sympy import *
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
+from matplotlib.widgets import Button
+import ipywidgets as widgets
+from lab_utils_backprop import *
