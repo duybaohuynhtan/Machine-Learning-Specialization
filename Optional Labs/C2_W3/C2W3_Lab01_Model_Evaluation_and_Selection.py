91f5222abd1890ddef05005e09a015504af358f5
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-16 11:41:56 +0700

    C2_W3_Lab01

--- Optional Labs/C2_W3/C2W3_Lab01_Model_Evaluation_and_Selection.py
+++ Optional Labs/C2_W3/C2W3_Lab01_Model_Evaluation_and_Selection.py
@@ -19,3 +19,38 @@ np.set_printoptions(precision=2)
 # suppress warnings
 tf.get_logger().setLevel('ERROR')
 tf.autograph.set_verbosity(0)
+
+# Load the dataset from the text file
+data = np.loadtxt('./data/data_w3_ex1.csv', delimiter=',')
+
+# Split the inputs and outputs into separate arrays
+x = data[:,0]
+y = data[:,1]
+
+# Convert 1-D arrays into 2-D because the commands later will require it
+x = np.expand_dims(x, axis=1)
+y = np.expand_dims(y, axis=1)
+
+print(f"the shape of the inputs x is: {x.shape}")
+print(f"the shape of the targets y is: {y.shape}")
+
+# Plot the entire dataset
+utils.plot_dataset(x=x, y=y, title="input vs. target")
+
+# Get 60% of the dataset as the training set. Put the remaining 40% in temporary variables: x_ and y_.
+x_train, x_, y_train, y_ = train_test_split(x, y, test_size=0.40, random_state=1)
+
+# Split the 40% subset above into two: one half for cross validation and the other for the test set
+x_cv, x_test, y_cv, y_test = train_test_split(x_, y_, test_size=0.50, random_state=1)
+
+# Delete temporary variables
+del x_, y_
+
+print(f"the shape of the training set (input) is: {x_train.shape}")
+print(f"the shape of the training set (target) is: {y_train.shape}\n")
+print(f"the shape of the cross validation set (input) is: {x_cv.shape}")
+print(f"the shape of the cross validation set (target) is: {y_cv.shape}\n")
+print(f"the shape of the test set (input) is: {x_test.shape}")
+print(f"the shape of the test set (target) is: {y_test.shape}")
+
+utils.plot_train_cv_test(x_train, y_train, x_cv, y_cv, x_test, y_test, title="input vs. target")
