a2f1021cc43ae9656cc7dd3dd4452550e30111b1
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-16 12:01:50 +0700

    C2_W3_Lab01

--- Optional Labs/C2_W3/C2W3_Lab01_Model_Evaluation_and_Selection.py
+++ Optional Labs/C2_W3/C2W3_Lab01_Model_Evaluation_and_Selection.py
@@ -203,3 +203,75 @@ test_mse = mean_squared_error(y_test, yhat) / 2
 print(f"Training MSE: {train_mses[degree-1]:.2f}")
 print(f"Cross Validation MSE: {cv_mses[degree-1]:.2f}")
 print(f"Test MSE: {test_mse:.2f}")
+
+# Add polynomial features
+degree = 1
+poly = PolynomialFeatures(degree, include_bias=False)
+X_train_mapped = poly.fit_transform(x_train)
+X_cv_mapped = poly.transform(x_cv)
+X_test_mapped = poly.transform(x_test)
+
+# Scale the features using the z-score
+scaler = StandardScaler()
+X_train_mapped_scaled = scaler.fit_transform(X_train_mapped)
+X_cv_mapped_scaled = scaler.transform(X_cv_mapped)
+X_test_mapped_scaled = scaler.transform(X_test_mapped)
+
+# Initialize lists that will contain the errors for each model
+nn_train_mses = []
+nn_cv_mses = []
+
+# Build the models
+nn_models = utils.build_models()
+
+# Loop over the the models
+for model in nn_models:
+    
+    # Setup the loss and optimizer
+    model.compile(
+    loss='mse',
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
+    )
+
+    print(f"Training {model.name}...")
+    
+    # Train the model
+    model.fit(
+        X_train_mapped_scaled, y_train,
+        epochs=300,
+        verbose=0
+    )
+    
+    print("Done!\n")
+
+    
+    # Record the training MSEs
+    yhat = model.predict(X_train_mapped_scaled)
+    train_mse = mean_squared_error(y_train, yhat) / 2
+    nn_train_mses.append(train_mse)
+    
+    # Record the cross validation MSEs 
+    yhat = model.predict(X_cv_mapped_scaled)
+    cv_mse = mean_squared_error(y_cv, yhat) / 2
+    nn_cv_mses.append(cv_mse)
+
+    
+# print results
+print("RESULTS:")
+for model_num in range(len(nn_train_mses)):
+    print(
+        f"Model {model_num+1}: Training MSE: {nn_train_mses[model_num]:.2f}, " +
+        f"CV MSE: {nn_cv_mses[model_num]:.2f}"
+        )
+    
+# Select the model with the lowest CV MSE
+model_num = 3
+
+# Compute the test MSE
+yhat = nn_models[model_num-1].predict(X_test_mapped_scaled)
+test_mse = mean_squared_error(y_test, yhat) / 2
+
+print(f"Selected Model: {model_num}")
+print(f"Training MSE: {nn_train_mses[model_num-1]:.2f}")
+print(f"Cross Validation MSE: {nn_cv_mses[model_num-1]:.2f}")
+print(f"Test MSE: {test_mse:.2f}")
