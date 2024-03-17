6a580a68e2333aaf3b8d19686e0551da50990473
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-17 23:50:53 +0700

    C2_W4_Lab01

--- Optional Labs/C2_W4/C2_W4_Lab01_Decision_Trees.py
+++ Optional Labs/C2_W4/C2_W4_Lab01_Decision_Trees.py
@@ -4,3 +4,16 @@ import matplotlib.pyplot as plt
 from utils import *
 
 _ = plot_entropy()
+
+X_train = np.array([[1, 1, 1],
+[0, 0, 1],
+ [0, 1, 0],
+ [1, 0, 1],
+ [1, 1, 1],
+ [1, 1, 0],
+ [0, 0, 0],
+ [1, 1, 0],
+ [0, 1, 0],
+ [0, 1, 0]])
+
+y_train = np.array([1, 1, 0, 0, 1, 1, 0, 1, 0, 0])
