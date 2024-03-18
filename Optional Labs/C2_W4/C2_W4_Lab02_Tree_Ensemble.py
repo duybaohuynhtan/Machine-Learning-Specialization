acecf4e93836b084993696b757083937727a2fb6
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-18 21:44:21 +0700

    C2_W4_Lab02

--- Optional Labs/C2_W4/C2_W4_Lab02_Tree_Ensemble.py
+++ Optional Labs/C2_W4/C2_W4_Lab02_Tree_Ensemble.py
@@ -12,3 +12,30 @@ RANDOM_STATE = 55 ## You will pass it to every sklearn call so we ensure reprodu
 
 # Load the dataset using pandas
 df = pd.read_csv("heart.csv")
+
+df.head()
+
+cat_variables = ['Sex',
+'ChestPainType',
+'RestingECG',
+'ExerciseAngina',
+'ST_Slope'
+]
+
+# This will replace the columns with the one-hot encoded ones and keep the columns outside 'columns' argument as it is.
+df = pd.get_dummies(data = df,
+                         prefix = cat_variables,
+                         columns = cat_variables)
+
+df.head()
+
+var = [x for x in df.columns if x not in 'HeartDisease'] ## Removing our target variable
+
+print(len(var))
+
+X_train, X_test, y_train, y_test = train_test_split(df[var], df['HeartDisease'], train_size = 0.8, random_state = RANDOM_STATE)
+
+# We will keep the shuffle = True since our dataset has not any time dependency.
+
+print(f'train samples: {len(X_train)}\ntest samples: {len(X_test)}')
+print(f'target proportion: {sum(y_train)/len(y_train):.4f}')
