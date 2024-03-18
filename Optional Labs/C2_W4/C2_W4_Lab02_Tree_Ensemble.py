9cbdc2397ae64ca8fe1723be578c3ac11d6b1eb9
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-18 08:44:47 +0700

    C2_W4_Lab02

---
+++ Optional Labs/C2_W4/C2_W4_Lab02_Tree_Ensemble.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+
+RANDOM_STATE = 55 ## You will pass it to every sklearn call so we ensure reproducibility
+
+# Load the dataset using pandas
+df = pd.read_csv("heart.csv")
