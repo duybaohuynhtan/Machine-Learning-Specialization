26295529573a823e3537ad7e7f18dc2cd66282a5
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-19 17:36:08 +0700

    C2_W4_Lab02

--- Optional Labs/C2_W4/C2_W4_Lab02_Tree_Ensemble.py
+++ Optional Labs/C2_W4/C2_W4_Lab02_Tree_Ensemble.py
@@ -90,3 +90,77 @@ decision_tree_model = DecisionTreeClassifier(min_samples_split = 50,
                                              random_state = RANDOM_STATE).fit(X_train,y_train)
 
 print(f"Metrics train:\n\tAccuracy score: {accuracy_score(decision_tree_model.predict(X_train),y_train):.4f}\nMetrics test:\n\tAccuracy score: {accuracy_score(decision_tree_model.predict(X_test),y_test):.4f}")
+
+min_samples_split_list = [2,10, 30, 50, 100, 200, 300, 700]  ## If the number is an integer, then it is the actual quantity of samples,
+                                             ## If it is a float, then it is the percentage of the dataset
+max_depth_list = [2, 4, 8, 16, 32, 64, None]
+n_estimators_list = [10,50,100,500]
+
+accuracy_list_train = []
+accuracy_list_test = []
+for min_samples_split in min_samples_split_list:
+    # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
+    model = RandomForestClassifier(min_samples_split = min_samples_split,
+                                   random_state = RANDOM_STATE).fit(X_train,y_train) 
+    predictions_train = model.predict(X_train) ## The predicted values for the train dataset
+    predictions_test = model.predict(X_test) ## The predicted values for the test dataset
+    accuracy_train = accuracy_score(predictions_train,y_train)
+    accuracy_test = accuracy_score(predictions_test,y_test)
+    accuracy_list_train.append(accuracy_train)
+    accuracy_list_test.append(accuracy_test)
+
+plt.title('Train x Test metrics')
+plt.xlabel('min_samples_split')
+plt.ylabel('accuracy')
+plt.xticks(ticks = range(len(min_samples_split_list )),labels=min_samples_split_list) 
+plt.plot(accuracy_list_train)
+plt.plot(accuracy_list_test)
+plt.legend(['Train','Test'])
+
+accuracy_list_train = []
+accuracy_list_test = []
+for max_depth in max_depth_list:
+    # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
+    model = RandomForestClassifier(max_depth = max_depth,
+                                   random_state = RANDOM_STATE).fit(X_train,y_train) 
+    predictions_train = model.predict(X_train) ## The predicted values for the train dataset
+    predictions_test = model.predict(X_test) ## The predicted values for the test dataset
+    accuracy_train = accuracy_score(predictions_train,y_train)
+    accuracy_test = accuracy_score(predictions_test,y_test)
+    accuracy_list_train.append(accuracy_train)
+    accuracy_list_test.append(accuracy_test)
+
+plt.title('Train x Test metrics')
+plt.xlabel('max_depth')
+plt.ylabel('accuracy')
+plt.xticks(ticks = range(len(max_depth_list )),labels=max_depth_list)
+plt.plot(accuracy_list_train)
+plt.plot(accuracy_list_test)
+plt.legend(['Train','Test'])
+
+accuracy_list_train = []
+accuracy_list_test = []
+for n_estimators in n_estimators_list:
+    # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
+    model = RandomForestClassifier(n_estimators = n_estimators,
+                                   random_state = RANDOM_STATE).fit(X_train,y_train) 
+    predictions_train = model.predict(X_train) ## The predicted values for the train dataset
+    predictions_test = model.predict(X_test) ## The predicted values for the test dataset
+    accuracy_train = accuracy_score(predictions_train,y_train)
+    accuracy_test = accuracy_score(predictions_test,y_test)
+    accuracy_list_train.append(accuracy_train)
+    accuracy_list_test.append(accuracy_test)
+
+plt.title('Train x Test metrics')
+plt.xlabel('n_estimators')
+plt.ylabel('accuracy')
+plt.xticks(ticks = range(len(n_estimators_list )),labels=n_estimators_list)
+plt.plot(accuracy_list_train)
+plt.plot(accuracy_list_test)
+plt.legend(['Train','Test'])
+
+random_forest_model = RandomForestClassifier(n_estimators = 100,
+                                             max_depth = 8, 
+                                             min_samples_split = 10).fit(X_train,y_train)
+
+print(f"Metrics train:\n\tAccuracy score: {accuracy_score(random_forest_model.predict(X_train),y_train):.4f}\nMetrics test:\n\tAccuracy score: {accuracy_score(random_forest_model.predict(X_test),y_test):.4f}")
