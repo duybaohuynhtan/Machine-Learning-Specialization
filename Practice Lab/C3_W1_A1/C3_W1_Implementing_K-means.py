1872e0b54c1716485015f586b54113af9b8173d1
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-20 13:52:35 +0700

    C3_W1_A1

--- Practice Lab/C3_W1_A1/C3_W1_Implementing_K-means.py
+++ Practice Lab/C3_W1_A1/C3_W1_Implementing_K-means.py
@@ -1,3 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
+
+def find_closest_centroids(X, centroids):
+    """
+    Computes the centroid memberships for every example
+    
+    Args:
+        X (ndarray): (m, n) Input values      
+        centroids (ndarray): k centroids
+    
+    Returns:
+        idx (array_like): (m,) closest centroids
+    
+    """
+
+    # Set K
+    K = centroids.shape[0]
+
+    idx = np.zeros(X.shape[0], dtype=int)
+
+    for i in range(X.shape[0]):
+          # Array to hold distance between X[i] and each centroids[j]
+          distance = [] 
+          for j in range(centroids.shape[0]):
+              norm_ij = np.linalg.norm(X[i] - centroids[j])
+              distance.append(norm_ij)
+
+          idx[i] = np.argmin(distance)
+    
+    return idx
+
+def compute_centroids(X, idx, K):
+    """
+    Returns the new centroids by computing the means of the 
+    data points assigned to each centroid.
+    
+    Args:
+        X (ndarray):   (m, n) Data points
+        idx (ndarray): (m,) Array containing index of closest centroid for each 
+                       example in X. Concretely, idx[i] contains the index of 
+                       the centroid closest to example i
+        K (int):       number of centroids
+    
+    Returns:
+        centroids (ndarray): (K, n) New centroids computed
+    """
+    
+    m, n = X.shape
+    
+    centroids = np.zeros((K, n))
+    
+    for k in range(K):   
+          points = X[idx == k]  
+          centroids[k] = np.mean(points, axis = 0)
+  
+    return centroids
+
+# Load an example dataset that we will be using
+X = load_data()
