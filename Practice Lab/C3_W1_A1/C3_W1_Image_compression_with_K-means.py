08807ae300986688451f6fb4bb65add6e5a45c24
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-21 15:10:26 +0700

    C3_W1_A1

--- Practice Lab/C3_W1_A1/C3_W1_Image_compression_with_K-means.py
+++ Practice Lab/C3_W1_A1/C3_W1_Image_compression_with_K-means.py
@@ -109,3 +109,50 @@ def kMeans_init_centroids(X, K):
     centroids = X[randidx[:K]]
     
     return centroids
+
+# Load an image of a bird
+original_img = plt.imread('bird_small.png')
+
+# Visualizing the image
+plt.imshow(original_img)
+
+# Divide by 255 so that all values are in the range 0 - 1
+original_img = original_img / 255
+
+# Reshape the image into an m x 3 matrix where m = number of pixels
+# (in this case m = 128 x 128 = 16384)
+# Each row will contain the Red, Green and Blue pixel values
+# This gives us our dataset matrix X_img that we will use K-Means on.
+
+X_img = np.reshape(original_img, (original_img.shape[0] * original_img.shape[1], 3))
+
+# Run your K-Means algorithm on this data
+# You should try different values of K and max_iters here
+K = 16                       
+max_iters = 10               
+
+# Using the function you have implemented above. 
+initial_centroids = kMeans_init_centroids(X_img, K) 
+
+# Run K-Means - this takes a couple of minutes
+centroids, idx = run_kMeans(X_img, initial_centroids, max_iters)
+
+# Represent image in terms of indices
+X_recovered = centroids[idx, :] 
+
+# Reshape recovered image into proper dimensions
+X_recovered = np.reshape(X_recovered, original_img.shape) 
+
+# Display original image
+fig, ax = plt.subplots(1,2, figsize=(8,8))
+plt.axis('off')
+
+ax[0].imshow(original_img*255)
+ax[0].set_title('Original')
+ax[0].set_axis_off()
+
+
+# Display compressed image
+ax[1].imshow(X_recovered*255)
+ax[1].set_title('Compressed with %d colours'%K)
+ax[1].set_axis_off()
