b6923a7dbcab36819593c8b14b9d35f686ec3982
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-23 19:36:06 +0700

    C3_W2_A1

---
+++ Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from recsys_utils import *
+
+#Load data
+X, W, b, num_movies, num_features, num_users = load_precalc_params_small()
+Y, R = load_ratings_small()
+
+print("Y", Y.shape, "R", R.shape)
+print("X", X.shape)
+print("W", W.shape)
+print("b", b.shape)
+print("num_features", num_features)
+print("num_movies",   num_movies)
+print("num_users",    num_users)
