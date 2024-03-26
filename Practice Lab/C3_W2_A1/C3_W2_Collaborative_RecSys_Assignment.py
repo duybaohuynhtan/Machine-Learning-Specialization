af31e13295b53cc48e4f1c2d921655140f773c6b
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-03-26 22:29:01 +0700

    C3_W2_A1

--- Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
+++ Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
@@ -60,3 +60,34 @@ print(f"Cost: {J:0.2f}")
 # Evaluate cost function with regularization 
 J = cofi_cost_func_v(X_r, W_r, b_r, Y_r, R_r, 1.5);
 print(f"Cost (with regularization): {J:0.2f}")
+
+movieList, movieList_df = load_Movie_List_pd()
+
+my_ratings = np.zeros(num_movies)          #  Initialize my ratings
+
+# Check the file small_movie_list.csv for id of each movie in our dataset
+# For example, Toy Story 3 (2010) has ID 2700, so to rate it "5", you can set
+my_ratings[2700] = 5 
+
+#Or suppose you did not enjoy Persuasion (2007), you can set
+my_ratings[2609] = 2;
+
+# We have selected a few movies we liked / did not like and the ratings we
+# gave are as follows:
+my_ratings[929]  = 5   # Lord of the Rings: The Return of the King, The
+my_ratings[246]  = 5   # Shrek (2001)
+my_ratings[2716] = 3   # Inception
+my_ratings[1150] = 5   # Incredibles, The (2004)
+my_ratings[382]  = 2   # Amelie (Fabuleux destin d'Amélie Poulain, Le)
+my_ratings[366]  = 5   # Harry Potter and the Sorcerer's Stone (a.k.a. Harry Potter and the Philosopher's Stone) (2001)
+my_ratings[622]  = 5   # Harry Potter and the Chamber of Secrets (2002)
+my_ratings[988]  = 3   # Eternal Sunshine of the Spotless Mind (2004)
+my_ratings[2925] = 1   # Louis Theroux: Law & Disorder (2008)
+my_ratings[2937] = 1   # Nothing to Declare (Rien à déclarer)
+my_ratings[793]  = 5   # Pirates of the Caribbean: The Curse of the Black Pearl (2003)
+my_rated = [i for i in range(len(my_ratings)) if my_ratings[i] > 0]
+
+print('\nNew user ratings:\n')
+for i in range(len(my_ratings)):
+    if my_ratings[i] > 0 :
+        print(f'Rated {my_ratings[i]} for  {movieList_df.loc[i,"title"]}');
