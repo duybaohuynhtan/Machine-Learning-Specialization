0a4125c62335a992c7ae8a04aee7e56f22e4fd7c
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-01 23:21:57 +0700

    C3_W2_A1

--- Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
+++ Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
@@ -3,6 +3,39 @@ import tensorflow as tf
 from tensorflow import keras
 from recsys_utils import *
 
+# GRADED FUNCTION: cofi_cost_func
+# UNQ_C1
+
+def cofi_cost_func(X, W, b, Y, R, lambda_):
+    """
+    Returns the cost for the content-based filtering
+    Args:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters
+      Y (ndarray (num_movies,num_users)    : matrix of user ratings of movies
+      R (ndarray (num_movies,num_users)    : matrix, where R(i, j) = 1 if the i-th movies was rated by the j-th user
+      lambda_ (float): regularization parameter
+    Returns:
+      J (float) : Cost
+    """
+    nm, nu = Y.shape
+    J = 0
+    ### START CODE HERE ###  
+    for j in range(nu):
+        w = W[j,:]
+        b_j = b[0,j]
+        for i in range(nm):
+            x = X[i,:]
+            y = Y[i,j]
+            r = R[i,j]
+            J += r * np.square((np.dot(w,x) + b_j - y ))
+    J += (lambda_) * (np.sum(np.square(W)) + np.sum(np.square(X)))
+    J = J/2
+    ### END CODE HERE ### 
+
+    return J
+
 #Load data
 X, W, b, num_movies, num_features, num_users = load_precalc_params_small()
 Y, R = load_ratings_small()
