7f2a3a0b56ba53517a678c636985bfe12f21cd76
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-02 23:30:23 +0900

    C3_W2_A1

--- Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
+++ Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
@@ -51,3 +51,18 @@ print("num_users",    num_users)
 #  From the matrix, we can compute statistics like average rating.
 tsmean =  np.mean(Y[0, R[0, :].astype(bool)])
 print(f"Average rating for movie 1 : {tsmean:0.3f} / 5" )
+
+# Reduce the data set size so that this runs faster
+num_users_r = 4
+num_movies_r = 5 
+num_features_r = 3
+
+X_r = X[:num_movies_r, :num_features_r]
+W_r = W[:num_users_r,  :num_features_r]
+b_r = b[0, :num_users_r].reshape(1,-1)
+Y_r = Y[:num_movies_r, :num_users_r]
+R_r = R[:num_movies_r, :num_users_r]
+
+# Evaluate cost function
+J = cofi_cost_func(X_r, W_r, b_r, Y_r, R_r, 0);
+print(f"Cost: {J:0.2f}")
