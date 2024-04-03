dfd9ff88f4fbed0686bbc14f623cd2f690951024
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-03 19:14:32 +0900

    C3_W2_A1

--- Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
+++ Practice Lab/C3_W2_A1/C3_W2_Collaborative_RecSys_Assignment.py
@@ -3,14 +3,39 @@ import tensorflow as tf
 from tensorflow import keras
 from recsys_utils import *
 
+def cofi_cost_func(X, W, b, Y, R, lambda_):
+    """
+    Returns the cost for the content-based filtering
+    Args:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters
+      Y (ndarray (num_movies,num_users)    : matrix of user ratings of movies
+      R (ndarray (num_movies,num_users)    : matrix, where R(i, j) = 1 if the i-th movies was rated by the j-th user
+      lambda_ (float): regularization parameter
+    Returns:
+      J (float) : Cost
+    """
+    nm, nu = Y.shape
+    J = 0
+    
+    for j in range(nu):
+        w = W[j,:]
+        b_j = b[0,j]
+        for i in range(nm):
+            x = X[i,:]
+            y = Y[i,j]
+            r = R[i,j]
+            J += r * np.square((np.dot(w,x) + b_j - y ))
+    J += (lambda_) * (np.sum(np.square(W)) + np.sum(np.square(X)))
+    J = J/2
+    
+    return J
+
 #Load data
 X, W, b, num_movies, num_features, num_users = load_precalc_params_small()
 Y, R = load_ratings_small()
 
-print("Y", Y.shape, "R", R.shape)
-print("X", X.shape)
-print("W", W.shape)
-print("b", b.shape)
-print("num_features", num_features)
-print("num_movies",   num_movies)
-print("num_users",    num_users)
+#  From the matrix, we can compute statistics like average rating.
+tsmean =  np.mean(Y[0, R[0, :].astype(bool)])
+print(f"Average rating for movie 1 : {tsmean:0.3f} / 5" )
