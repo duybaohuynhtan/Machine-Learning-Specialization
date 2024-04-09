a7fa95904e067efb6868682f6a07b0ccc38c42ed
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-09 23:41:56 +0900

    C3_W2_A2

--- Practice Lab/C3_W2_A2/C3_W2_RecSysNN.py
+++ Practice Lab/C3_W2_A2/C3_W2_RecSysNN.py
@@ -8,3 +8,7 @@ from sklearn.model_selection import train_test_split
 import tabulate
 from recsysNN_utils import *
 pd.set_option("display.precision", 1)
+
+top10_df = pd.read_csv("./data/content_top10_df.csv")
+bygenre_df = pd.read_csv("./data/content_bygenre_df.csv")
+top10_df
