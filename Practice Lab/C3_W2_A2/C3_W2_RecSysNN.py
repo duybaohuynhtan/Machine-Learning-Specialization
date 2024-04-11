5140c589940a158cfff924935efdeb89c3f7c23c
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-11 10:32:20 +0900

    C3_W2_A2

--- Practice Lab/C3_W2_A2/C3_W2_RecSysNN.py
+++ Practice Lab/C3_W2_A2/C3_W2_RecSysNN.py
@@ -9,8 +9,12 @@ import tabulate
 from recsysNN_utils import *
 pd.set_option("display.precision", 1)
 
-top10_df = pd.read_csv("./data/content_top10_df.csv")
-bygenre_df = pd.read_csv("./data/content_bygenre_df.csv")
-top10_df
+# Load Data, set configuration variables
+item_train, user_train, y_train, item_features, user_features, item_vecs, movie_dict, user_to_genre = load_data()
 
-bygenre_df
+num_user_features = user_train.shape[1] - 3  # remove userid, rating count and ave rating during training
+num_item_features = item_train.shape[1] - 1  # remove movie id at train time
+uvs = 3  # user genre vector start
+ivs = 3  # item genre vector start
+u_s = 3  # start of columns to use in training, user
+i_s = 1  # start of columns to use in training, items
