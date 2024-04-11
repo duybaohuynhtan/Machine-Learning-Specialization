d434ff009b9fb514235cae9fdcfb0d3ca8667b38
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-11 10:35:15 +0900

    C3_W2_A2

--- Practice Lab/C3_W2_A2/C3_W2_RecSysNN.py
+++ Practice Lab/C3_W2_A2/C3_W2_RecSysNN.py
@@ -18,3 +18,25 @@ uvs = 3  # user genre vector start
 ivs = 3  # item genre vector start
 u_s = 3  # start of columns to use in training, user
 i_s = 1  # start of columns to use in training, items
+
+# scale training data
+item_train_unscaled = item_train
+user_train_unscaled = user_train
+y_train_unscaled    = y_train
+
+scalerItem = StandardScaler()
+scalerItem.fit(item_train)
+item_train = scalerItem.transform(item_train)
+
+scalerUser = StandardScaler()
+scalerUser.fit(user_train)
+user_train = scalerUser.transform(user_train)
+
+scalerTarget = MinMaxScaler((-1, 1))
+scalerTarget.fit(y_train.reshape(-1, 1))
+y_train = scalerTarget.transform(y_train.reshape(-1, 1))
+#ynorm_test = scalerTarget.transform(y_test.reshape(-1, 1))
+
+item_train, item_test = train_test_split(item_train, train_size=0.80, shuffle=True, random_state=1)
+user_train, user_test = train_test_split(user_train, train_size=0.80, shuffle=True, random_state=1)
+y_train, y_test       = train_test_split(y_train,    train_size=0.80, shuffle=True, random_state=1)
