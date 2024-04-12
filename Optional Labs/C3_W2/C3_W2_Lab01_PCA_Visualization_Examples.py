f23827976279e5fed685272c8c956deb5bdaca5f
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-12 11:13:57 +0900

    C3_W2_Lab01

---
+++ Optional Labs/C3_W2/C3_W2_Lab01_PCA_Visualization_Examples.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from pca_utils import plot_widget
+from bokeh.io import show, output_notebook
+from bokeh.plotting import figure
+import matplotlib.pyplot as plt
+import plotly.offline as py
