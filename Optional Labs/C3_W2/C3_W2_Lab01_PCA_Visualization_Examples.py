6b10d0970a6b3d8396375e2d0367f4b98cdf4b95
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-13 11:26:17 +0900

    C3_W2_Lab01

--- Optional Labs/C3_W2/C3_W2_Lab01_PCA_Visualization_Examples.py
+++ Optional Labs/C3_W2/C3_W2_Lab01_PCA_Visualization_Examples.py
@@ -9,3 +9,32 @@ import plotly.offline as py
 
 py.init_notebook_mode()
 output_notebook()
+
+X = np.array([[ 99,  -1],
+       [ 98,  -1],
+       [ 97,  -2],
+       [101,   1],
+       [102,   1],
+       [103,   2]])
+plt.plot(X[:,0], X[:,1], 'ro')
+
+# Loading the PCA algorithm
+pca_2 = PCA(n_components=2)
+pca_2
+
+# Let's fit the data. We do not need to scale it, since sklearn's implementation already handles it.
+pca_2.fit(X)
+
+pca_2.explained_variance_ratio_
+
+X_trans_2 = pca_2.transform(X)
+X_trans_2
+
+pca_1 = PCA(n_components=1)
+pca_1
+
+pca_1.fit(X)
+pca_1.explained_variance_ratio_
+
+X_trans_1 = pca_1.transform(X)
+X_trans_1
