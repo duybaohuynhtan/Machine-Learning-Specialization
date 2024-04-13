05296ccfca04c719c67326a35cd0b97c7d6b088b
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-13 11:28:40 +0900

    C3_W2_Lab01

--- Optional Labs/C3_W2/C3_W2_Lab01_PCA_Visualization_Examples.py
+++ Optional Labs/C3_W2/C3_W2_Lab01_PCA_Visualization_Examples.py
@@ -6,6 +6,20 @@ from bokeh.io import show, output_notebook
 from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 import plotly.offline as py
+from pca_utils import random_point_circle, plot_3d_2d_graphs
+
+def get_pairs(n = 100):
+    from random import randint
+    i = 0
+    tuples = []
+    while i < 100:
+        x = df.columns[randint(0,999)]
+        y = df.columns[randint(0,999)]
+        while x == y or (x,y) in tuples or (y,x) in tuples:
+            y = df.columns[randint(0,999)]
+        tuples.append((x,y))
+        i+=1
+    return tuples
 
 py.init_notebook_mode()
 output_notebook()
@@ -79,8 +93,6 @@ show(p)
 
 plot_widget()
 
-from pca_utils import random_point_circle, plot_3d_2d_graphs
-
 X = random_point_circle(n = 150)
 
 deb = plot_3d_2d_graphs(X)
@@ -90,19 +102,6 @@ deb.update_layout(yaxis2 = dict(title_text = 'test', visible=True))
 df = pd.read_csv("toy_dataset.csv")
 
 df.head()
-
-def get_pairs(n = 100):
-    from random import randint
-    i = 0
-    tuples = []
-    while i < 100:
-        x = df.columns[randint(0,999)]
-        y = df.columns[randint(0,999)]
-        while x == y or (x,y) in tuples or (y,x) in tuples:
-            y = df.columns[randint(0,999)]
-        tuples.append((x,y))
-        i+=1
-    return tuples
 
 pairs = get_pairs()
 
