c331013d4b002e09d70d745ea6ab2922390f1268
Duy Bao Huynh Tan <duybaohuynhtan@gmail.com>
2024-04-14 20:01:07 +0900

    C3_W3_Lab01

---
+++ Practice Lab/C3_W3_A1/Deep_Q-Learning_Lunar_Lander.py
@@ -0,0 +1,103 @@
+import time
+from collections import deque, namedtuple
+
+import gym
+import numpy as np
+import PIL.Image
+import tensorflow as tf
+import utils
+
+from pyvirtualdisplay import Display
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.losses import MSE
+from tensorflow.keras.optimizers import Adam
+
+def compute_loss(experiences, gamma, q_network, target_q_network):
+    """ 
+    Calculates the loss.
+    
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+      q_network: (tf.keras.Sequential) Keras model for predicting the q_values
+      target_q_network: (tf.keras.Sequential) Keras model for predicting the targets
+          
+    Returns:
+      loss: (TensorFlow Tensor(shape=(0,), dtype=int32)) the Mean-Squared Error between
+            the y targets and the Q(s,a) values.
+    """
+
+    # Unpack the mini-batch of experience tuples
+    states, actions, rewards, next_states, done_vals = experiences
+    
+    # Compute max Q^(s,a)
+    max_qsa = tf.reduce_max(target_q_network(next_states), axis=-1)
+    
+    # Set y = R if episode terminates, otherwise set y = R + γ max Q^(s,a).
+    ### START CODE HERE ### 
+    y_targets = rewards + (gamma * max_qsa * (1 - done_vals))
+    ### END CODE HERE ###
+    
+    # Get the q_values and reshape to match y_targets
+    q_values = q_network(states)
+    q_values = tf.gather_nd(q_values, tf.stack([tf.range(q_values.shape[0]),
+                                                tf.cast(actions, tf.int32)], axis=1))
+        
+    # Compute the loss
+    loss = MSE(y_targets, q_values)
+    
+    return loss
+
+@tf.function
+def agent_learn(experiences, gamma):
+    """
+    Updates the weights of the Q networks.
+    
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+    
+    """
+    
+    # Calculate the loss
+    with tf.GradientTape() as tape:
+        loss = compute_loss(experiences, gamma, q_network, target_q_network)
+
+    # Get the gradients of the loss with respect to the weights.
+    gradients = tape.gradient(loss, q_network.trainable_variables)
+    
+    # Update the weights of the q_network.
+    optimizer.apply_gradients(zip(gradients, q_network.trainable_variables))
+
+    # update the weights of target q_network
+    utils.update_target_network(q_network, target_q_network)
+
+# Set up a virtual display to render the Lunar Lander environment.
+Display(visible=0, size=(840, 480)).start();
+
+# Set the random seed for TensorFlow
+tf.random.set_seed(utils.SEED)
+
+MEMORY_SIZE = 100_000     # size of memory buffer
+GAMMA = 0.995             # discount factor
+ALPHA = 1e-3              # learning rate  
+NUM_STEPS_FOR_UPDATE = 4  # perform a learning update every C time steps
+
+# Create the Q-Network
+q_network = Sequential([
+    Input(shape=state_size),                      
+    Dense(units=64, activation='relu'),            
+    Dense(units=64, activation='relu'),            
+    Dense(units=num_actions, activation='linear'),
+    ])
+
+# Create the target Q^-Network
+target_q_network = Sequential([
+    Input(shape=state_size),                      
+    Dense(units=64, activation='relu'),            
+    Dense(units=64, activation='relu'),            
+    Dense(units=num_actions, activation='linear'),
+    ])
+
+optimizer = Adam(learning_rate=ALPHA)
